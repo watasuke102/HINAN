776423f8fe4c91d2291b2e01473d803aef1c1eb9
watasuke102 <watasuke102@gmail.com>
2021-02-25 13:18:47 +0900

    update: Add help

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#include <QDebug>
+#include <string.h>
 
-int main() {
+void Help() {
+  qDebug() << "usage: ./hinan <option>";
+  qDebug() << " Options: -h | --help   Show this help message.";
+  qDebug() << "\n";
+}
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    Help();
+    return 1;
+  }
+  if (strcmp(argv[1], "--help") || strcmp(argv[1], "-h")) {
+    Help();
+    return 0;
+  }
+  qDebug() << argv[1];
   return 0;
 }
