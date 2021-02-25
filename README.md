1319a6fe6fe0118545f9a431a504caee440c8737
watasuke102 <watasuke102@gmail.com>
2021-02-25 12:57:47 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# HINAN - HINAN Is Not AN h8 emulator
+H8 practice kit simulator
+
+## What's this
+This software simulate H8 microcomputer practice kit.  
+**This software doesn't emulate H8 microcomputer.**
+
+## LICENSE
+Mozilla Public License v2.0
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// HINAN - simulate H8 microcomputer practice kit
+// main.cpp
+//
+// CopyRight (c) 2021 Watasuke
+// Email  : <watasuke102@gmail.com>
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+int main() {
+  return 0;
+}
